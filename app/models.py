183bae65667a00ab43947111c336c2cf46bbe957
youkelike <815666124@qq.com>
2016-11-07 22:45:15 +0800

    搭建项目框架

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=32)
+    user_groups = models.ManyToManyField('UserGroup')
+    friends = models.ManyToManyField('self',related_name='my_friends',blank=True,symmetrical=False)
+    def __str__(self):
+        return self.name
+
+class UserGroup(models.Model):
+    name = models.CharField(max_length=32,unique=True)
+
+    def __str__(self):
+        return self.name
+
+class QQGroup(models.Model):
+    name = models.CharField(max_length=64)
+    founder = models.ForeignKey(UserProfile)
+    brief = models.TextField(max_length=1024,default='nothing...')
+    admin = models.ManyToManyField(UserProfile,related_name='group_admins')
+    members = models.ManyToManyField(UserProfile,related_name='group_members')
+    member_limit = models.IntegerField(default=20)
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+@login_required
+def index(request):
+    return render(request,'index.html')
+
+def acc_login(request):
+    if request.method == 'POST':
+        print(request.POST)
+        #验证动作
+        user = authenticate(username=request.POST.get('username'),password=request.POST.get('password'))
+        if user is not None:
+            #登录动作
+            login(request,user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            login_err = 'Wrong username or password'
+            return render(request, 'login.html',{'login_err':login_err})
+    return render(request,'login.html')
+
+def acc_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def dashboard(request):
+
+    return render(request,'webqq/dashboard.html')
